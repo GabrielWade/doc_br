9d52e0a22dcc930655ce059a95334e51d228f106
Gabriel Lima <gablima004@gmail.com>
2023-05-07 16:39:51 -0300

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+Meus desafios de criação de sistemas que validam documentos do Brasil, nesse desafio baixamos bibliotecas e consumimos API de CEP.
---
+++ main.py
@@ -0,0 +1,28 @@
+# from cpf_cnpj import Documento
+# from telefones_br import TelefonesBr
+# from datas_br import DatasBr
+# from datetime import datetime, timedelta
+# from acesso_cep import Buscaendereco
+# import requests
+
+# cpf_um = Cpf("16836337765")
+# print(cpf_um)
+# exemplo_cnpJ = "35379838000112"
+# documento = Documento.criar_documento(exemplo_cnpJ)
+# print(documento)
+
+# telefone = "552126481234"
+# telefone_objeto = TelefonesBr(telefone)
+# print(telefone_objeto)
+
+# hoje = DatasBr()
+# print(hoje.tempo_cadastro())
+# hoje = datetime.today()
+# amanha = datetime.today() + timedelta(days=1)
+# print(hoje - amanha)
+
+# cep = "01001000"
+# objeto_cep = Buscaendereco(cep)
+# print(objeto_cep)
+# bairro, cidade, uf = objeto_cep.acessa_via_cep()
+# print(bairro, cidade, uf)
